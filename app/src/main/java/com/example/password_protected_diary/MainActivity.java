eccfe71eb91a58e547b38e59bf2c0b2e04fb2fe0
Anthony <ac161@my.yorku.ca>
2022-03-16 19:09:43 -0400

    new commit a

--- app/src/main/java/com/example/password_protected_diary/MainActivity.java
+++ app/src/main/java/com/example/password_protected_diary/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //tsests
+        asda
         setContentView(R.layout.activity_main);
     }
 }
