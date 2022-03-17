0581d400dd0c446bc776fa0b2a8866b7d81c8fd3
Anthony <ac161@my.yorku.ca>
2022-03-16 21:07:43 -0400

    added 3 screens

--- app/src/main/java/com/example/password_protected_diary/MainActivity.java
+++ app/src/main/java/com/example/password_protected_diary/MainActivity.java
@@ -1,16 +1,37 @@
 package com.example.password_protected_diary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //tsests
-        asda
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginButtonClicked(View v){
+        TableLayout loginSignUp = (TableLayout) findViewById(R.id.loginSignUp);
+        loginSignUp.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void signUpButtonClicked(View v){
+        ConstraintLayout loginSignUp = (ConstraintLayout) findViewById(R.id.loginSignUp);
+        ConstraintLayout register = (ConstraintLayout) findViewById(R.id.registerLayout);
+        loginSignUp.setVisibility(View.INVISIBLE);
+        register.setVisibility(View.VISIBLE);
+    }
+    public void registerButtonClicked(View v) {
+        ConstraintLayout register = (ConstraintLayout) findViewById(R.id.registerLayout);
+        ConstraintLayout firstScreen = (ConstraintLayout) findViewById(R.id.firstScreen);
+        register.setVisibility(View.INVISIBLE);
+        firstScreen.setVisibility(View.VISIBLE);
     }
 }
